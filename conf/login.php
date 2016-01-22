595b1bab76388a05b3391c8818863599f7e296d2
cah4ec84@gmail.com <GHpr0ject>
2016-01-22 16:13:16 +0300

    GetInfo()  Функиця для получения данных и отрисовки любых данных запрашиваемых через VIEW

--- conf/login.php
+++ conf/login.php
@@ -8,4 +8,8 @@
  */
 ini_set('mssql.charset','windows-1252');
 $serverName='meddb';
-$connectionInfo=array('Database'=>'upi_0_2','UID'=>'convertor','PWD'=>'Br@inFuck','CharacterSet' => 'UTF-8');
+$connectionInfo=array('Database'=>'upi_0_2',
+    'UID'=>'convertor',
+    'PWD'=>'Br@inFuck',
+    'CharacterSet' => 'UTF-8',
+    'ReturnDatesAsStrings'=>true);
--- php/getInfo.php
+++ php/getInfo.php
@@ -4,16 +4,60 @@
  * User: Alexander
  * Date: 21.01.2016
  * Time: 16:18
+ * При открытии сайта JS вызывает данный модуль, последовательно передавая ему имена вкладок.
+ * Модуль заполняет данными все необходимые вкладки.
+ * Далее если пользователь желает уточнить данные то в данный скрипт передаются запрашиваемые данные.
  */
 require_once '../conf/login.php';
 $mssqlConn=sqlsrv_connect($serverName,$connectionInfo);
-$currentTab = $_POST['menu-div']; //Активная вкладка
-$checkboxs = $_POST['maincontainer-checkbox_details']; //выбранные для для уточнения
+if( $mssqlConn === false ) die( print_r( sqlsrv_errors(), true));
+//Получаем полную информацию для каждой вкладки.
+//$tab = $_POST['tab'];
+$tab = 'firmsInfo';
 
-$inset = 'subs';
+//Функиция получения данных и отрисовки таблицы
+GetInfo($mssqlConn,$tab);
 
 
 
-$mode = 'subs'; //запрос информации о подписках
+//Для уточнения данных передается активная вкладка и выбранные на ней элементы.
+$currentTab = $_POST['maincontainerCurrentTab']; //Активная вкладка
+$checkboxs = $_POST['maincontainerCheckboxDetails']; //выбранные для для уточнения
+
+
+//Получания всех данных из таблицы, и списка её полей
+function GetInfo($conn,$table) {
+    //Заголовок таблицы
+    $queryFields="Select COLUMN_NAME from INFORMATION_SCHEMA.COLUMNS  where TABLE_NAME='".$table."'";
+    $resultFields=sqlsrv_query($conn,$queryFields) or die( print_r( sqlsrv_errors(), true));
+    $tableHead= '<table border="1"> <thead><tr>';
+    $columnCount=0;
+    while ($row=sqlsrv_fetch_array($resultFields,SQLSRV_FETCH_NUMERIC)) {
+        $tableHead.='<td>'.$row[0].'</td>';
+        $columnCount++;
+    };
+    $tableHead.='</tr></thead>';
+
+    //Данные в таблице
+    $queryData="select * from ". $table." order by 1";
+    $resultData=sqlsrv_query($conn,$queryData) or die( print_r( sqlsrv_errors(), true));
+    $tableBody='<tbody>';
+        while ($row2=sqlsrv_fetch_array($resultData,SQLSRV_FETCH_NUMERIC)) {
+        $tableBody.='<tr>';
+            for ($j=0; $j<$columnCount; $j++) {
+            $tableBody.='<td>'.$row2[$j].'</td>';
+        }
+        $tableBody.='</tr>';
+    };
+    $tableBody.='</tbody></table>';
+
+//Отрисовка таблицы
+    $tableHead.=$tableBody;
+    echo $tableHead;
+
+}
+
+
+
 
 
