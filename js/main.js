d1a64a4b67a5d9379a5e1790eb10ddda3e2f38cb
cah4ec84@gmail.com <GHpr0ject>
2016-02-04 10:48:49 +0300

    Написал многоколоночную фильрацию на JS

--- js/main.js
+++ js/main.js
@@ -5,73 +5,44 @@
 
 $(document).ready(function () {
     console.log ("DOM loaded.Jquery-2.1.4");
-    //Запрос полных данных из БД для всех вкладок.
+//Запрос полных данных из БД для всех вкладок.
     var tabs = $('input[name="maincontainer-radio"]');
     for (var i=0;i<tabs.length;i++) {
         console.log('Try to ask '+tabs[i].id);
         getInfo(tabs[i].id);
     }
+//
 
-    //Добавляем функцию поиска в каждую вкладку
+//Добавляем функцию поиска в каждую вкладку
     $(".inputFilter").keyup(debounce(function makeSearchableTabs () {
-        console.log('Enter FN');
-
         var tabSelector=
             ".maincontainer > #subsInfo:checked ~ .maincontainer-subsInfo," +
             ".maincontainer > #firmsInfo:checked ~ .maincontainer-firmsInfo," +
             ".maincontainer > #moreInfo:checked ~ .maincontainer-moreInfo";
-        //Все видимые строки таблицы
         //var trNotHidden= $("tbody tr",tabSelector).not('[style="display: none;"]')
+        //Все видимые строки таблицы
         var trAll = $("tbody tr",tabSelector);
-
-        if ( $(".filtred").length) {
-            console.log ('Found previous Filter');
-            trAll = $(".filtred",tabSelector);
-        } else {
-            console.log ('NO FILTERS');
-        }
+        //скрываем всю таблицу
+        trAll.hide()
 
         //Фильтры для данной таблицы
         var searchInputs = $(".inputFilter",tabSelector);
 
-//Собираем инфу о фильрах
-        var searchValArr=[];
-        for (var i=0; i<searchInputs.length;i++) {
-            if( $(searchInputs[i]).children().val() ) { //если стоит фильтр
-                searchValArr[i]=$(searchInputs[i]).children().val();
-                console.log(searchValArr);
-            }
-        }
-        console.log (trAll);
-//Фильтруем данные
-        trAll.hide() //скрываем всю таблицу
-        for (var i=0;i<searchValArr.length;i++) {
-            if (searchValArr[i]) {
-                var colIndex = $(searchInputs[i]).index() + 1; //Узнаем номер столбца таблицы
+        //Фильтруем данные
+        var filtredClass='';
+        searchInputs.each(function () {
+            if ( $(this).children().val() ) {
+                var colIndex = ($(this).index() + 1).toString(); //Узнаем номер столбца таблицы
+                trAll.removeClass('filtredCol'+colIndex); //Удаляем ранее установленный на него фильтр
                 var colData = $("td:nth-child(" + colIndex + ")", trAll); //выбираем все  данные столбца
-                colData.filter(":contains(" + searchValArr[i] + ")").parent("tr").addClass('filtred');
-                trAll= $(".filtred");
+                colData.filter(":contains(" + $(this).children().val() + ")").parent("tr").addClass('filtredCol'+colIndex);
+                filtredClass+='.filtredCol'+colIndex;
             }
-        }
-        trAll.show();
-
-/*
-        //Фильруем данные
-        for (var i =0; i<searchInputs.length; i++) {
-            if( $(searchInputs[i]).children().val() ) { //если стоит фильтр
-                var searchVal = $(searchInputs[i]).children().val().toLowerCase() //переводим строку поиска в нижний регистр
-                var colIndex = $(searchInputs[i]).index() + 1; //Узнаем номер столбца
-                var colData = $("td:nth-child(" + colIndex + ")", trAll); //выбираем все  данные столбца из видимых строк
-                trAll.hide(); //Скрываем все данные
-                colData.filter(":contains(" + searchVal + ")").parent("tr").show();
-                useFilter=true;
-            }
-            if (!useFilter) {         //если нет фильтра то отображаем все строки
-                trAll.show();
-            }
-        }
-*/
-
+        });
+        //Выводим только отфильрованные данные
+        if (filtredClass) {
+            $(filtredClass).show();
+        } else {trAll.show()};
     },500));
 
 
