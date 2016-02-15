4ba27abedbb9b3f62392fd2c1466e9b30ba6b121
cah4ec84@gmail.com <GHpr0ject>
2016-02-15 14:17:23 +0300

    Переделал фильрацию. Приступил к запросу дополнительной инфы из бд по клику на искомой строчке.

--- js/main.js
+++ js/main.js
@@ -3,51 +3,87 @@
  При загрузке получаем список вкладок и асинхронно заполняем их информацией
 */
 
+// при загрузке страницы запрос полных данных из БД для всех вкладок.
 $(document).ready(function () {
-    console.log ("DOM loaded.Jquery-2.1.4");
-//Запрос полных данных из БД для всех вкладок.
+    console.log("DOM loaded.Jquery-2.1.4");
+
     var tabs = $('input[name="maincontainer-radio"]');
-    for (var i=0;i<tabs.length;i++) {
-        console.log('Try to ask '+tabs[i].id);
+    for (var i = 0; i < tabs.length; i++) {
+        console.log('Try to ask ' + tabs[i].id);
         getInfo(tabs[i].id);
     }
-//
 
-//Добавляем функцию поиска в каждую вкладку
-    $(".inputFilter").keyup(debounce(function makeSearchableTabs () {
-        var tabSelector=
-            ".maincontainer > #subsInfo:checked ~ .maincontainer-subsInfo," +
-            ".maincontainer > #firmsInfo:checked ~ .maincontainer-firmsInfo," +
-            ".maincontainer > #moreInfo:checked ~ .maincontainer-moreInfo";
-        //var trNotHidden= $("tbody tr",tabSelector).not('[style="display: none;"]')
-        //Все видимые строки таблицы
-        var trAll = $("tbody tr",tabSelector);
-        //скрываем всю таблицу
-        trAll.hide()
+    //для каждой вкладки, в её основную таблицу, в строки поиска, добавляем функцию поиска по мере набора текста.
+    $(".maincontainer>input").each (function () {
+        $(".inputFilter",$('div[class$='+this.id+']')).click( function() {
+            //Функция фильтрации данных при поиске.;
+        //Для каждой кнопки поиска опеределяем её родительскую таблицу.
+            var parentTable = $(this).parent().parent().parent();
+            var trAll = $("tbody tr",parentTable);
+        //Передаем в функцию обработки нажатия кнопок.
+            var searchInputs = $(".inputFilter",parentTable);
 
-        //Фильтры для данной таблицы
-        var searchInputs = $(".inputFilter",tabSelector);
-
-        //Фильтруем данные
-        var filtredClass='';
-        searchInputs.each(function () {
-            if ( $(this).children().val() ) {
-                var colIndex = ($(this).index() + 1).toString(); //Узнаем номер столбца таблицы
-                trAll.removeClass('filtredCol'+colIndex); //Удаляем ранее установленный на него фильтр
-                var colData = $("td:nth-child(" + colIndex + ")", trAll); //выбираем все  данные столбца
-                colData.filter(":contains(" + $(this).children().val() + ")").parent("tr").addClass('filtredCol'+colIndex);
-                filtredClass+='.filtredCol'+colIndex;
+            //Фильтры для данной таблицы обнуляются
+            for (var i=0; i<searchInputs.length;i++) {
+                trAll.removeClass('filtredCol'+(i+1))
             }
-        });
-        //Выводим только отфильрованные данные
-        if (filtredClass) {
-            $(filtredClass).show();
-        } else {trAll.show()};
-    },500));
+            var filtredClass='';
+            $(".inputFilter",parentTable).keyup(debounce(function () {
+                trAll.hide()
+                //Фильтруем данные
+                searchInputs.each(function () {
+                    if ( $(this).children().val() ) {
+                        var colIndex = ($(this).index() + 1).toString(); //Узнаем номер столбца таблицы
+                        trAll.removeClass('filtredCol'+colIndex); //Удаляем ранее установленный на него фильтр
+                        var colData = $("td:nth-child(" + colIndex + ")", trAll); //выбираем все  данные столбца
+                        colData.filter(":contains(" + $(this).children().val().toLowerCase() + ")").parent("tr").addClass('filtredCol'+colIndex);
+                        filtredClass+='.filtredCol'+colIndex;
+                    }
+                });
+
+                //Выводим только отфильрованные данные
+                if (filtredClass) {
+                    $(filtredClass).show();
+                } else {trAll.show()};
+            },300));
+        })
+    })
+
+    //Событие для клика по строчкам таблицы - получение дополнительной информации из представлений в БД
+    $("tbody tr").click( function () {
+    //Функция переноса данных из строки таблицы в окна фильтрации
+        //Передаем данные по кликнутой строке в строки поиска и скрываем всю таблицу
+        var pT=$(this).parent().parent()
+        $("td",this).each (function() {
+            $( $(".inputFilter",pT)[$(this).index()]).children().val($(this).text() )
+        })
+        $("tbody tr",pT).hide();
+
+        //Отправка уточняющего запроса к БД
+        //Передаем текущую таблицу и поле ИД выбранной строчки
+        getInfoDetail( $("input:checked").attr('id'), $(".inputFilter",pT).children().val() );
+    })
 
 
-    });
-//Запрос данных из БД для каждой вкладки
+
+});
+
+
+
+
+// ФУНКЦИИ
+
+//Функция задержки
+function debounce(fn, duration) {
+    var timer;
+    return function () {
+        clearTimeout(timer);
+        timer = setTimeout(fn, duration)
+    }
+}
+
+
+//Функция запроса данных из БД для каждой вкладки
 function getInfo(tab) {
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange=function () {
@@ -62,14 +98,15 @@ function getInfo(tab) {
 }
 
 
-//Функция задержки
-function debounce(fn, duration) {
-    var timer;
-    return function() {
-        clearTimeout(timer);
-        timer = setTimeout(fn, duration)
-    }
+//Функция уточняющего запроса к БД
+function getInfoDetail(tab,queryId) {
+    console.log("getInfoDetail");
+    console.log(tab);
+    console.log(queryId);
 }
+
+
+
 
 
 
